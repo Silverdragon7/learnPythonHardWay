e39361a95a171d6bed8f5568d2f5d8c6522f312c
Mateusz Bebak <mateusz.bebak@g2a.local@2WAN00268-20.g2a.local>
2021-01-19 16:45:07 +0100

    dodanie pliku 13.1

---
+++ ex13.1.py
@@ -0,0 +1,7 @@
+from sys import argv
+one, two = argv
+
+print("pierwsza zmienna argv: ", one)
+print("druga zmienna argv: ", two)
+three = input("Podaj 3 zmienną: ")
+print("trzecia zmienna to: ", three)
