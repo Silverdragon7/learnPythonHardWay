5c1f6eb5b6d9b0dec0dd16ef6ffa04fac17ca6ed
Silverdragon7 <mateusz.bebak@g2a.local>
2021-01-20 15:43:08 +0100

    file ex17.py - shorter version

--- ex17.py
+++ ex17.py
@@ -2,25 +2,14 @@ from sys import argv
 from os.path import exists
 
 script, from_file, to_file = argv
-
 print(f"Copying from {from_file} to {to_file}")
+indata = open(from_file).read()
 
-#we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
-
-print(f"The input file is {len(indata)} bytes long")
-print(f"Does the output file exist? {exists(to_file)}")
 if exists(to_file) == True:
-    print("Ready, hit RETURN to continue, CTRL_C to abort.")
-    input()
-
     out_file = open(to_file, 'w')
     out_file.write(indata)
-
-    print("Alright, all done.")
+    print("Done.")
 
     out_file.close()
-    in_file.close()
 elif exists(to_file) == False:
-    print("Output file doesn't exist.")
+    exit()
