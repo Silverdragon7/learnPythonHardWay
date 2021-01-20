f1d2c5dff37c61234372a1be74011234e79c6790
Silverdragon7 <mateusz.bebak@g2a.local>
2021-01-20 13:27:41 +0100

    repair exs1.py (error msg + text format)

--- exs1.py
+++ exs1.py
@@ -1,5 +1,9 @@
-#skrypt z możliwością wyboru
 from sys import argv
+
+if(len(argv) == 1):
+    print("Podaj nazwę bohatera jako pierwszy argument.")
+    exit()
+
 skrypt, nazwaBohatera = argv
 print(f"Witaj w grze {skrypt}!. Twoja nazwa to {nazwaBohatera}. Czy chcesz kontynuować?(Yes/No)")
 a = input("> ")
@@ -7,70 +11,70 @@ a = input("> ")
 if a == "Yes":
     print("Wspaniale. Ruszajmy więc po przygodę!")
     print("""
-    Rok 1023 II ery
-    Godzina 12.00
-    Przechadzasz się ulicą tętniącego życiem miasteczka. Nagle ktoś mija cię z biegnąc w przeciwną stronę.
-    Decydujesz się za nim podążyć?(Yes/No)
+    \rRok 1023 II ery
+    \rGodzina 12.00
+    \rPrzechadzasz się ulicą tętniącego życiem miasteczka. Nagle ktoś mija cię z biegnąc w przeciwną stronę.
+    \rDecydujesz się za nim podążyć?(Yes/No)
     """)
     b = input("> ")
 
     if b == "Yes":
         print("""
-        Podążasz za człowiekiem bięgnącym coraz wolniej, jakby uważał że zmylił pościg. 
-        Skręca w mniej ruchliwą uliczkę na przedmieściach.
-        W końcu zatrzymuje się w jakimś zaułku. Wyglada że cię nie zauważył.
-        Stoi do ciebie tyłem i przesuwa palcami po ścianie budynku jakby czegoś szukał.
-        Zastanawiasz się czy podejście do niego będzie dobtym pomysłem.
-        Może lepiej pozostać w ukryciu.
-        Chcesz podejść do nieznajomego?(Yes/No)
+        \rPodążasz za człowiekiem bięgnącym coraz wolniej, jakby uważał że zmylił pościg. 
+        \rSkręca w mniej ruchliwą uliczkę na przedmieściach.
+        \rW końcu zatrzymuje się w jakimś zaułku. Wyglada że cię nie zauważył.
+        \rStoi do ciebie tyłem i przesuwa palcami po ścianie budynku jakby czegoś szukał.
+        \rZastanawiasz się czy podejście do niego będzie dobtym pomysłem.
+        \rMoże lepiej pozostać w ukryciu.
+        \rChcesz podejść do nieznajomego?(Yes/No)
         """)
         c = input("> ")
             #ścieżka do zrobienia
         print("""
-        Ta ścieżka nie jest jeszcze gotowa... dzięki za przetestowanie dema ;)
+        \rTa ścieżka nie jest jeszcze gotowa... dzięki za przetestowanie dema ;)
         """)
 
     elif b == "No":
         print("""
-        Wzruszasz ramionami i idziesz dalej ulicą jakby nic się nie stało. 
-        Po chwili zatrzymuje cię paru strażników, którzy wyglądają jakby kogoś szukali.
-        Chcesz im opowiedzieć o człowieku który cię minął?(Yes/No)
+        \rWzruszasz ramionami i idziesz dalej ulicą jakby nic się nie stało. 
+        \rPo chwili zatrzymuje cię paru strażników, którzy wyglądają jakby kogoś szukali.
+        \rChcesz im opowiedzieć o człowieku który cię minął?(Yes/No)
         """)
         c = input("> ")
 
         if c == "Yes":
             print("""
-            Wskazujesz strażnikom drogę, a oni ruszają w pościg za nieznajomym.
-            Widzisz kątem oka że na dachu coś się poruszyło.
-            Wzruszasz ramionami ruszając w stronę domu.
-            Otwierasz drzwi i przechodzisz przed próg.
-            Nagle czujesz jak coś wbija się w twoje ciało.
-            Siły opuszczają twoje ciało a z szyji cieknie krew.
-            Przed tobą stoi człowiek podobny do tego który cię mijał.
-            Trzyma w ręce małą kuszę z której do ciebie strzelił.
-            - Zły wybór - mówi. - Oczy widzą, uszy słyszą, usta milczą.
-            Nieznajomy wychodzi podczas gdy umierasz na podłodze swojego pokoju.
+            \rWskazujesz strażnikom drogę, a oni ruszają w pościg za nieznajomym.
+            \rWidzisz kątem oka że na dachu coś się poruszyło.
+            \rWzruszasz ramionami ruszając w stronę domu.
+            \rOtwierasz drzwi i przechodzisz przed próg.
+            \rNagle czujesz jak coś wbija się w twoje ciało.
+            \rSiły opuszczają twoje ciało a z szyji cieknie krew.
+            \rPrzed tobą stoi człowiek podobny do tego który cię mijał.
+            \rTrzyma w ręce małą kuszę z której do ciebie strzelił.
+            \r- Zły wybór - mówi. - Oczy widzą, uszy słyszą, usta milczą.
+            \rNieznajomy wychodzi podczas gdy umierasz na podłodze swojego pokoju.
             """)
             print("Koniec gry")
 
         elif c == "No":
             print("""
-            Ta ścieżka nie jest jeszcze gotowa... dzięki za przetestowanie dema ;)
+            \rTa ścieżka nie jest jeszcze gotowa... dzięki za przetestowanie dema ;)
             """)
             #wątek drugi
 
         else:
-            print("Wybrano odpowiedź z poza zakresu.")
+            print("\rWybrano odpowiedź z poza zakresu.")
             print("Koniec gry")
 
 
     else:
-        print("Wybrano odpowiedź z poza zakresu.")
+        print("\rWybrano odpowiedź z poza zakresu.")
         print("Koniec gry")
 
 elif a == "No":
     print("Żegnaj")
 
 else:
-    print("Wybrano odpowiedź z poza zakresu.")
+    print("\rWybrano odpowiedź z poza zakresu.")
     print("Koniec gry")
