78eb33b98cbe5544bfa58bf5e1c921ab6afac127
Silverdragon7 <mateusz.bebak@g2a.local>
2021-01-27 17:06:08 +0100

    ex41 , ex42 ex3 is not working

--- exs3.py
+++ exs3.py
@@ -1,4 +1,5 @@
 import random
+import uuid
 from sys import argv
 
 class Karta:
@@ -59,15 +60,16 @@ class Karta:
 
 
 class Gracz:
-    def __init__(self):
+    def __init__(self, numer_gracza):
         self.karty = []
         self.oznaczenia_kart = []
         self.wartosci = []
         self.najwyzsza_wartosc = None
         self.najwyzsza_karta = None
+        self.aktualna_karta = None
+        self.nazwa_gracza = "Gracz_" + str(numer_gracza)
         self.podaj_karty()
         self.wybierz_najwyzsza()
-        self.usun_najwyzsza()
 
     def podaj_karty(self):
         for x in range(0, 5):
@@ -78,14 +80,14 @@ class Gracz:
         for aktualna_karta in self.karty:
             self.wartosci.append(Karta.pokaz_wartosc_bojowa(aktualna_karta))
         self.wartosci.sort()
-        self.najwyzsza_wartosc = self.wartosci[-1] #DODAĆ WYBIERANIE NAJWIĘKSZEJ KARTY NIE WARTOŚCI
+        self.najwyzsza_wartosc = self.wartosci[-1]
         self.karty.sort(key=lambda x: getattr(x, "wartosc_bojowa"))
         self.najwyzsza_karta = self.karty[-1]
 
     def pokaz_karty(self):
         for karta in self.karty:
             self.oznaczenia_kart.append({karta.pokaz_wartosc():karta.pokaz_kolor()})
-        return self.oznaczenia_kart # POPRAWIĆ
+        return self.oznaczenia_kart
 
     def pokaz_najwyzsza(self):
         return (self.najwyzsza_karta).pokaz_wartosc() + " " + (self.najwyzsza_karta).pokaz_kolor()
@@ -93,41 +95,88 @@ class Gracz:
     def pokaz_najwyzsza_wartosc(self):
         return self.najwyzsza_wartosc
 
-    def usun_najwyzsza(self):
+    def pokaz_wybarna_karte(self, karta):
+        self.karty.sort(key=lambda x: getattr(x, "wartosc_bojowa"))
+        self.aktualna_karta = self.karty[karta]
+
+    def usun_ostatnia(self):
         self.karty.pop()
-        self.wybierz_najwyzsza()
+
 
 class Gra:
     def __init__(self, ilosc_graczy):
-        self.
+        self.gracze = []
+        self.ilosc_graczy = ilosc_graczy
+        self.licznik_petli = 5
+        for x in range(0, ilosc_graczy):
+            self.gracze.append(Gracz(x))
+        self.porownanie()
+        for x in range(0, ilosc_graczy):
+            print(self.gracze[x].nazwa_gracza)
+            print(self.gracze[x].najwyzsza_karta.pokaz_kolor())
+            print(self.gracze[x].najwyzsza_karta.pokaz_wartosc())
+            print(self.gracze[x].pokaz_karty())
 
 
+    def porownanie(self):
+        # y = self.gracze[0].pokaz_najwyzsza_wartosc()
+        # for gracz in self.gracze:
+        #     z = gracz.pokaz_najwyzsza_wartosc()
+        #     if y > z:
+        #         self.gracze.remove(gracz)
+        #     elif y < z:
+        #         del self.gracze[0]
+        #         y = gracz.pokaz_najwyzsza_wartosc()
+        #     else:
+        #         self.gracze.remove(gracz) # zwrot złej wartości
+        # return
+        self.gracze.sort(key=lambda x: getattr(x, "najwyzsza_wartosc"))
+        gracze2 = []
+        self.licznik_petli -= 1
+        konflikt = 0
+        print("licznik")
+        print(self.licznik_petli)
+        for x in range(0, len(self.gracze)): # długość listy
+            if self.gracze[x].najwyzsza_wartosc == self.gracze[-1].najwyzsza_wartosc: # dlaczego licznik jest poza zasięgiem?? tak jakby w liście nie było tylu graczy
+                print("wchodzę do pętli")
+                gracze2.append(self.gracze[x]) # lista z graczami do 2 tury
+                self.gracze[x].pokaz_wybarna_karte(self.licznik_petli)
+                gracze2.sort(key=lambda p: getattr(p, "aktualna_karta"))
+                # zwrócić kartę i posortować je w liście karty2
+                self.gracze = gracze2
+                konflikt = 1
+        if konflikt != 1:
+            return self.gracze[-1].nazwa_gracza  # zwraca zanim wróci do pętli
+        else:
+            self.porownanie()
 
-def rozgrywka():
-
-    atut1 = player1.pokaz_najwyzsza_wartosc()
-    atut2 = player2.pokaz_najwyzsza_wartosc()
-    print(atut1)
-    print(atut2)
-    if atut1 > atut2:
-        print("Zwyciężył gracz 1: ", player1.pokaz_najwyzsza())
-    elif atut1 < atut2:
-        print("Zwyciężył gracz 2: ", player2.pokaz_najwyzsza())
-    else:
-        print("Remis")
+#
+# def rozgrywka():
+#
+#     atut1 = player1.pokaz_najwyzsza_wartosc()
+#     atut2 = player2.pokaz_najwyzsza_wartosc()
+#     print(atut1)
+#     print(atut2)
+#     if atut1 > atut2:
+#         print("Zwyciężył gracz 1: ", player1.pokaz_najwyzsza())
+#     elif atut1 < atut2:
+#         print("Zwyciężył gracz 2: ", player2.pokaz_najwyzsza())
+#     else:
+#         print("Remis")
 
 def main():
-    global player1
-    global player2
-    player1 = Gracz()
-    player2 = Gracz()
-    print("Karty pierwszego gracza: ", player1.pokaz_karty())
-    print("Karty drugiego gracza: ", player2.pokaz_karty())
-    # print(player1.pokaz_najwyzsza())
-    # test = Karta()
-    # print(test.pokaz_wartosc())
-    # wartosci = (wartosc_karty(zamiana_znaku_na_wartosc(karty1)))
-    # wartosci1 = (wartosc_karty(zamiana_znaku_na_wartosc(karty2)))
-    rozgrywka()
+    global liczba_graczy
+    skrypt, liczba_graczy = argv
+    rozgrywka = Gra(10)
+    print("Zwyciężył gracz: ")
+    print(rozgrywka.porownanie())
+    # print("Karty pierwszego gracza: ", player1.pokaz_karty())
+    # print("Karty drugiego gracza: ", player2.pokaz_karty())
+    # # print(player1.pokaz_najwyzsza())
+    # # test = Karta()
+    # # print(test.pokaz_wartosc())
+    # # wartosci = (wartosc_karty(zamiana_znaku_na_wartosc(karty1)))
+    # # wartosci1 = (wartosc_karty(zamiana_znaku_na_wartosc(karty2)))
+    # rozgrywka()
 
 main()
