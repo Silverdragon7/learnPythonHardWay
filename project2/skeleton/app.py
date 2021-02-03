0b42eb09ee54bca3272109c5addab65cb6e81401
Silverdragon7 <mateusz.bebak@g2a.local>
2021-02-03 12:29:07 +0100

    working web game

--- project2/skeleton/app.py
+++ project2/skeleton/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-from bin.test import funkcja
+from bin.engine import *
+from docs import *
 
 app = Flask(__name__)
 
@@ -10,13 +11,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
 @app.route("/start", methods=['POST', 'GET'])
-def index():
+def start():
     if request.method == "POST":
         choice = request.form['choice']
-        return render_template("start.html", text=funkcja(choice)) #funkcja tuuu
+        return render_template("start.html", text=main(choice)) #funkcja tuuu
     else:
-        return render_template("start.html", text="tekst początkowy")
+        return render_template("start.html", text="""Rok 1023 II ery
+            Godzina 12.00
+            Przechadzasz się ulicą tętniącego życiem miasteczka. Nagle ktoś mija cię z biegnąc w przeciwną stronę.
+            Decydujesz się za nim podążyć?""")
+
 
 if __name__ == "__main__":
     app.run()
