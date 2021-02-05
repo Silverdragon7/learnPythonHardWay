a7c8dc07b07e5bc37bc71acb638330bca6c109cd
Silverdragon7 <mateusz.bebak@g2a.local>
2021-02-05 14:53:52 +0100

    strona typu peewee - nie działa

--- quiz/app.py
+++ quiz/app.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
 # quiz/app.py
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask, g
+from peewee import SqliteDatabase
 
 app = Flask(__name__)
 
 # konfiguracja aplikacji
 app.config.update(dict(
     SECRET_KEY='bardzosekretnawartosc',
+    TYTUL='Quiz ORM Peewee',
     DATABASE=os.path.join(app.root_path, 'quiz.db'),
-    SQLALCHEMY_DATABASE_URI='sqlite:///' +
-                            os.path.join(app.root_path, 'quiz.db'),
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    TYTUL='Quiz SQLAlchemy'
 ))
 
 # tworzymy instancję bazy używanej przez modele
-baza = SQLAlchemy(app)
+baza = SqliteDatabase(app.config['DATABASE'])
+
+
+@app.before_request
+def before_request():
+    g.db = baza
+    g.db.get_conn()
+
+
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
